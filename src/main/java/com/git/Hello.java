1af193f83f6bcc54a225504f3127b284316968ca
wayy <xf@wayy.fun>
2022-08-21 08:56:24 +0800

    first commit

---
+++ src/main/java/com/git/Hello.java
@@ -0,0 +1,15 @@
+package com.git;
+
+/**
+ * @program: git-ignore-test
+ * @description:
+ * @author: Chunming Feng
+ * @email: xf@wayy.fun
+ * @create: 2022-08-21 08:52
+ **/
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
